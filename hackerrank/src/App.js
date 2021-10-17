861449dc1c7bf093f8b9710323dbe9e7dd4bb640
aditya <aditya29ks@gmail.com>
2021-10-17 17:07:11 +0530

    footer

--- hackerrank/src/App.js
+++ hackerrank/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Header from "./Core/Header.js"
+import Home from "./Core/Home.js"
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -15,7 +16,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={Header} />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </div>
---
+++ hackerrank/src/Core/Footer.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import "./Footer.css";
+import logo from "../Assets/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-main">
+        <div className="footer-wrapper">
+          <Row className="footer-row-main">
+            <Col>
+              <div className="footer-col-wrapper">
+                <div className="footer-col-heading">PRODUCTS</div>
+                <div className="footer-col-elements">
+                  <ul>
+                    <li>Remote First Hiring</li>
+                    <li>Remote First Planning</li>
+                    <li>Remote First Sourcing</li>
+                    <li>Remote First Screening</li>
+                    <li>Remote First Interviewing</li>
+                    <li>Remote First Ranking</li>
+                    <li>Insights</li>
+                    <li>HackerRank for Developers</li>
+                    <li>Partners</li>
+                    <li>Pricing</li>
+                  </ul>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="footer-col-wrapper">
+                <div className="footer-col-heading">SOLUTIONS</div>
+                <div className="footer-col-elements">
+                  <ul>
+                    <li>Remote Hiring</li>
+                    <li>University Recruiting</li>
+                    <li>Diversity & Inclusion</li>
+                    <li>Professional Hiring</li>
+                  </ul>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="footer-col-wrapper">
+                <div className="footer-col-heading">CUSTOMERS</div>
+                <div className="footer-col-elements">
+                  <ul>
+                    <li>All Industries</li>
+                    <li>Financial Services</li>
+                    <li>Technology</li>
+                    <li>More</li>
+                  </ul>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="footer-col-wrapper">
+                <div className="footer-col-heading">RESOURCES</div>
+                <div className="footer-col-elements">
+                  <ul>
+                    <li>Events</li>
+                    <li>Datasheets</li>
+                    <li>Guides</li>
+                    <li>Webinars</li>
+                    <li>Research</li>
+                    <li>Trust</li>
+                    <li>Security</li>
+                  </ul>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="footer-lastcol">
+                <div className="footer-lastcol01">
+                  
+                    <Col>
+                      <div className="footer-col-wrapper">
+                        <div className="footer-col-heading">PRODUCTS</div>
+                        <div className="footer-col-elements">
+                          <ul>
+                            <li>Attract</li>
+                            <li>Assess</li>
+                            <li>Hire</li>
+                            <li>Stories</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Col>
+                    <Col>
+                      <div className="footer-col-wrapper">
+                        <div className="footer-col-heading">PRODUCTS</div>
+                        <div className="footer-col-elements">
+                          <ul>
+                            <li>Insights</li>
+                            <li>HackerRank</li>
+                            <li>Partners</li>
+                            <li>Pricing</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </Col>
+                  
+                </div>
+
+                <div className="footer-lastcol02">
+                <div className="footer-lastcol02-icons">
+                  <img src={logo} alt="" />
+                  <i class="fab fa-facebook-square"></i>
+                  <i class="fab fa-instagram-square"></i>
+                  <i class="fab fa-twitter-square"></i>
+                  </div>
+                  <div>Privacy Policy</div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
